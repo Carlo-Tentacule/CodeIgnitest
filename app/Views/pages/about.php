3e4805dcd037e3804cc170f25e2fce31e97b3a75
Carlo-Tentacule <estebanhouillon@gmail.com>
2023-03-31 11:03:29 +0200

    initilisation codeigniter

---
+++ app/Views/pages/about.php
@@ -0,0 +1,5 @@
+<?php
+
+// 31/03/2023 10:30 ehn
+echo ("Je suis la vida loca");
+// 31/03/2023 10:30 ehn
---
+++ app/Views/pages/home.php
@@ -0,0 +1,9 @@
+<?php
+
+echo ("Je suis la page Home");
+
+
+// 
+// 31/03/2023 10:37 jfi
+echo ("je suis la page julien");
+// 31/03/2023 10:37 jfi
