59fdd42cab9220e160eade05ac1d12189e8b637a
Julien Franzi <julien-franzi@live.fr>
2023-03-31 12:02:23 +0200

    maj

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -7,3 +7,8 @@ echo ("Je suis la page Home");
 // 31/03/2023 10:37 jfi
 echo ("je suis la page julien");
 // 31/03/2023 10:37 jfi
+
+
+// 31/03/2023 11:48 jfi
+echo ("je suis un STRINGTORDU");
+// 31/03/2023 11:48 jfi
